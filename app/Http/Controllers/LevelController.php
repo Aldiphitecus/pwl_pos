ff3e62983013fffd901343db773dce67150369f7
Aldiphitecus <nurfahmialdi123@gmail.com>
2024-08-12 13:35:29 +0700

    Pertemuan 20 Percobaan 1: Implementasi DB Façade

---
+++ app/Http/Controllers/LevelController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
+
+class LevelController extends Controller
+{
+    public function index()
+    {
+        // DB::insert('insert into m_level (level_kode, level_nama, created_at) values (?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        // return 'Insert data berhasil';
+
+        // $row = DB::insert('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+        // return 'update data berhasil, jumlah data yang diupdate: ' . $row . 'baris';
+
+        // $row = DB::insert('delete from m_level where level_kode = ?', ['CUS']);
+        // return 'hapus data berhasil, jumlah data yang dihapus ' . $row . 'baris';
+
+        //menampilkan data
+        $data = DB::select('select * from m_level');
+        return view('level', ['data' => $data]);
+    }
+}
